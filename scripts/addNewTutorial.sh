2bbc05f809691ba1815ad9b5e886689782638ecf
BHT <toby@anl.gov>
2024-04-17 13:30:42 -0500

    run makeGitTutorial.py via addNewTutorial.sh

---
+++ scripts/addNewTutorial.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# to add a new tutorial
+# 1) create new directory in .../GSASII-tutorials
+# 2) add .html file, data/ directory
+# 3) add to git
+# 4) add to .../GSASII/tutorialIndex.py
+# 5) run this from file location (cd .../GSASII-tutorials/scripts; bash addNewTutorials.sh)
+#
+cp -p ../../GSASII/GSASII/tutorialIndex.py .
+python makeGitTutorial.py ..
+rm tutorialIndex.py
+echo use command:
+echo "    git add ../*/data/index.html ../tutorials.html"
