1daf0227707716d2a007af63bb9df6e689a5a757
BHT <toby@anl.gov>
2024-06-29 17:45:56 -0500

    run iconv on all files (see scripts/fixEncode.py). Failed on fullrmc-Ni/fullrmc-Ni.html & LabData/Laboratory X.htm

--- scripts/fixEncode.py
+++ scripts/fixEncode.py
@@ -1,12 +1,31 @@
 # scan through all tutorial files and change encoding
 import glob
-for f in glob.glob("../*/*.htm*"):
-    s = open(f,'rb').read()
-    if b'charset=windows-1252' in s:
-        print('Windows',f)
-        open(f,'wb').write(s.replace(b'charset=windows-1252',b'charset=UTF-8'))
-    elif b'charset=macintosh' in s:
-        open(f,'wb').write(s.replace(b'charset=macintosh',b'charset=UTF-8'))
-        print('Mac',f)
+#for f in glob.glob("../*/*.htm*"):
+#    s = open(f,'rb').read()
+#    if b'charset=windows-1252' in s:
+#        print('Windows',f)
+#        open(f,'wb').write(s.replace(b'charset=windows-1252',b'charset=UTF-8'))
+#    elif b'charset=macintosh' in s:
+#        open(f,'wb').write(s.replace(b'charset=macintosh',b'charset=UTF-8'))
+#        print('Mac',f)
+#    else:
+#        print('OK',f)
+
+import os
+import subprocess
+import glob
+failed = []
+f = os.path.expanduser('~/G2/git/GSASII-tutorials/RMCProfile-I/RMCProfile-I.htm')
+for f in glob.glob(os.path.expanduser('~/G2/git/GSASII-tutorials/*/*.htm*')):
+    ftmp = os.path.join('/tmp',os.path.split(f)[1])
+    cmd = f'iconv {f!r} -f windows-1252 -t utf-8 > {ftmp!r}'
+    print(cmd)
+    rc = subprocess.call(cmd,shell=True)
+    if rc == 0:
+        print(f'creation of {ftmp} OK, moving')
+        subprocess.run(['mv','-v',ftmp,f])
     else:
-        print('OK',f)
+        print(f'creation of {ftmp} Failed')
+        failed.append(f)
+print('failed on')
+for f in failed: print('\t',f)
